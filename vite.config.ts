bad733a9bbeb614285af89b19c8c5cfa88d61904
Manoj Padmanabha <mjgowda27g@gmail.com>
2025-07-28 01:18:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/', // 👈 use your repo name here
+  base: '/', // 👈 use your repo name here
   plugins: [react()],
 });
