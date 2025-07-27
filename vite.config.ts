3bbe7718ee7c2e0fbfcbbe78505841a575d1419b
Manoj Padmanabha <mjgowda27g@gmail.com>
2025-07-28 01:24:06 +0200

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // 👈 use your repo name here
+  base: 'https://manojpadmanabha.de/', // 👈 set this explicitly
   plugins: [react()],
 });
